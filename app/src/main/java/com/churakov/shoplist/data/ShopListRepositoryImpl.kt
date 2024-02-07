379107f54ea880aa2cc471ed16d0e4190e1a7831
churakovra <churakov.main@gmail.com>
2024-02-07 16:49:11 +0300

    Create ShopListRepositoryImpl

---
+++ app/src/main/java/com/churakov/shoplist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package com.churakov.shoplist.data
+
+import com.churakov.shoplist.domain.ShopItem
+import com.churakov.shoplist.domain.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl: ShopListRepository {
+
+    private val shoppingList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shoppingList.add(shopItem)
+    }
+
+    override fun removeShopItem(id: Int) {
+        shoppingList.removeAt(id)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldShopItem = getShopItem(shopItem.id)
+        shoppingList.remove(oldShopItem)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(id: Int): ShopItem {
+        return shoppingList.find {
+            it.id == id
+        } ?: throw RuntimeException("item with id $id is not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shoppingList.toList()
+    }
+}
--- app/src/main/java/com/churakov/shoplist/domain/ShopItem.kt
+++ app/src/main/java/com/churakov/shoplist/domain/ShopItem.kt
@@ -1,8 +1,12 @@
 package com.churakov.shoplist.domain
 
 data class ShopItem(
-    val id: Int,
     val value: String,
     val amount: String,
-    val signed: Boolean
-)
+    val signed: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
